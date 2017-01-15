153638aadf3c6deaa2813760a04eca2c9346d34b
knotsofunny <knots@desktop.home>
2017-01-15 16:32:11 -0500

    Wrong File

--- cave/cave_instant.js
+++ cave/cave_instant.js
@@ -6,23 +6,14 @@ var mid;
 
 var stack = [];
 
-var next;
-
 var prob = 0.65; //Probability of a cell to be converted
-var w = 15;
+var w = 9;
 var wallLimit = 4;
 var wallThickness = 3;
-var cavyness = 0.005;
+var cavyness = 0.01;
 var smoothness = 15;
-var fps = 60;
+var fps = 25;
 var count = 0;
-
-//Flags, because im bad at coding
-var startedMaze = false;
-var cleanup = false;
-
-
-
 
 function setup() {
     createCanvas(1500, 900);
@@ -39,9 +30,19 @@ function setup() {
 
 
     current = grid[index(1, 1)];
-    next = current.getRandomNeighbor();
     current.visited = true;
     current.wall = false;
+
+
+    createMaze();
+    randomizeMaze();
+    for (var i = 0; i < smoothness; i += 1) {
+        createRooms();
+        randomizeMaze();
+    }
+    prob = 0.95;
+    createRooms();
+    createRooms();
 
 
 }
@@ -54,61 +55,10 @@ function draw() {
     for (var i = 0; i < grid.length; i += 1) {
         grid[i].show();
     }
-
-
-
-
-
-    if (current != grid[index(1, 1)] || !startedMaze) {
-        startedMaze = true;
-        current.highlight();
-
-        if (next) {
-            stack.push(current);
-            next.visited = true;
-
-            if (current.i - next.i < 0) { //WORKS
-                for (var i = current.i; i < next.i; i += 1) {
-                    grid[index(i, current.j)].wall = false;
-                    grid[index(i, current.j)].visited = true;
-                }
-            } else if (current.i - next.i > 0) { //DOESNT WORK
-                for (var i = next.i; i < current.i; i += 1) {
-                    grid[index(i, current.j)].wall = false;
-                    grid[index(i, current.j)].visited = true;
-                }
-            } else if (current.j - next.j > 0) { //DOESNT WORK
-                for (var j = current.j; j > next.j; j -= 1) {
-                    grid[index(current.i, j)].wall = false;
-                    grid[index(current.i, j)].visited = true;
-                }
-            } else if (current.j - next.j < 0) { //WORKS
-                for (var j = next.j; j > current.j; j -= 1) {
-                    grid[index(current.i, j)].wall = false;
-                    grid[index(current.i, j)].visited = true;
-                }
-            }
-
-            current = next;
-            current.wall = false;
-
-        } else if (stack.length > 0) {
-            current = stack.pop();
-        }
-        next = current.getRandomNeighbor();
-    } else if (count < smoothness) {
-        frameRate(3);
-        randomizeMaze();
-        createRooms();
-        count += 1;
-    } else if (!cleanup) {
-        prob = 1;
-        createRooms();
-        frameRate(1);
-        cleanup = true;
-    } else {
-        frameRate(fps);
-    }
+    //  if (count < smoothness) {
+    //      createRooms();
+    //      count += 1;
+    //  }
 
 
 }
@@ -152,8 +102,54 @@ function createRooms() {
 
 
 function createMaze() {
+    var next = current.getRandomNeighbor();
+    while (next || stack.length > 0) {
+        if (current != grid[index(1, 1)]) {
+            current.highlight();
+        }
+        if (next) {
+            stack.push(current);
+            next.visited = true;
+
+            if (current.i - next.i < 0) { //WORKS
+                for (var i = current.i; i < next.i; i += 1) {
+                    grid[index(i, current.j)].wall = false;
+                    grid[index(i, current.j)].visited = true;
+                }
+            } else if (current.i - next.i > 0) { //DOESNT WORK
+                for (var i = next.i; i < current.i; i += 1) {
+                    grid[index(i, current.j)].wall = false;
+                    grid[index(i, current.j)].visited = true;
+                }
+            } else if (current.j - next.j > 0) { //DOESNT WORK
+                for (var j = current.j; j > next.j; j -= 1) {
+                    grid[index(current.i, j)].wall = false;
+                    grid[index(current.i, j)].visited = true;
+                }
+            } else if (current.j - next.j < 0) { //WORKS
+                for (var j = next.j; j > current.j; j -= 1) {
+                    grid[index(current.i, j)].wall = false;
+                    grid[index(current.i, j)].visited = true;
+                }
+            }
 
 
+            //  mid = grid[index((current.i + next.i) / 2, (current.j + next.j) / 2)]
+
+            current = next;
+            current.wall = false;
+
+
+
+
+            //  mid.wall = false;
+
+
+        } else if (stack.length > 0) {
+            current = stack.pop();
+        }
+        next = current.getRandomNeighbor();
+    }
 }
 
 function index(i, j) {
